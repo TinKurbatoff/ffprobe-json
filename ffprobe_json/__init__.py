990c4af28ade62f68aa20819f00b1f62f25daca7
TinKurbatoff <6782841@gmail.com>
2021-06-19 02:12:36 +0000

    added thumbnailing

--- ffprobe_json/__init__.py
+++ ffprobe_json/__init__.py
@@ -16,17 +16,43 @@ def ffprobe(file_path, quiet=True):
         if len(out)>0:
             result = json.loads(out)
         else:
-            result = {}
+            result = {}            
         err_json = {'error':err} if err else {'error':None}
+        # out.read()
+        # err.read()
         return result, err_json
     else:
         return {},{'error':'File not found'}
 
-def ffprobe_thumbnail(file_path, output_image=None):
+def ffprobe_thumbnail(file_path, output_image=None, seconds=0):
     if os.path.isfile(file_path):
         if not output_image:
-            output_image = f"{file_path}.jpg"    
-        subprocess.call(['ffmpeg', '-i', file_path, '-ss', '00:00:00.000', '-vframes', '1', output_image])
+            # output_image = f"{file_path}.jpg"    
+            output_image = f"{file_path}.gif"    
+        print('[FFTHUMB] output_image path:',output_image)
+        if (seconds < 3):
+            cl_length = seconds
+        else:
+            cl_length = 3
+        command_line = ['ffmpeg',
+                    '-nostdin',
+                    '-ss', f'{seconds}' ,
+                    '-v','quiet',
+                    '-t', f'{cl_length}' ,
+                    '-i', file_path, 
+                    '-vf', "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" ,
+                    '-loop', '0',
+                    output_image]
+        print(command_line)
+        # p = subprocess.Popen(['ffmpeg', '-i', file_path,'-v','quiet', '-an', '-ss', f'{seconds}', '-vframes', '1', output_image])
+        p = subprocess.Popen(command_line)
+        out, err =  p.communicate()
+        # out.read()
+        # err.read()
+        # print("====== OUT ======")
+        # print(out)        
+        # print("====== ERR ======")
+        # print(err)
         return output_image
     else:
         return {'error':'File not found'}
